32272e9dc0ffd0b53dc9f49ec217d22d33cac7f5
Marcus House <marcus@datawebnet.com.au>
2011-08-29 22:30:59 +1000

    -

--- core/components/formit_formbuilder/elements/snippets/snippet.formit_formbuilder.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once $modx->getOption('core_path',null,MODX_CORE_PATH).'components/formit_formbuilder/model/formit/formit_formbuilder.class.php';
-$o_formBuilder = new FormIt_FormBuilder($modx,$scriptProperties);
-
-return $o_formBuilder->output();
-?>
